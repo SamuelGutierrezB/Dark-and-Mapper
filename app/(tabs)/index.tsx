8692e20bf3472ffaf3526fd16fd060dec862558d
Beto <josealberto@ceti.mx>
2025-03-28 21:46:39 -0600

    add basic desing to login & add color & margines

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,105 +1,82 @@
-import { Image, StyleSheet, Platform } from "react-native";
+import { Image, StyleSheet, TextInput, TouchableOpacity, Text, View } from "react-native";
 
-import ParallaxScrollView from "@/components/ParallaxScrollView";
-import { ThemedText } from "@/components/ThemedText";
-import { ThemedView } from "@/components/ThemedView";
-
-export default function HomeScreen() {
+export default function LoginScreen() {
   return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: "#A1CEDC", dark: "#1D3D47" }}
-      headerImage={
-        <Image
-          source={require("@/assets/images/partial-react-logo.png")}
-          style={styles.reactLogo}
-        />
-      }
-    >
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Missions</ThemedText>
-        <ThemedText>
-          Holaaa Explore the dungeon missions and objectives to plan your next
-          move.
-        </ThemedText>
-      </ThemedView>
+    <View style={styles.container}>
+      <Text style={styles.title}>Inicia sesión</Text>
+      
+      <Text style={styles.label}>USUARIO:</Text>
+      <TextInput
+        style={styles.input}
+        placeholder="Ingresa tu nombre de usuario"
+        placeholderTextColor={"#AE9D7F"}
+      />
 
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Recommendations</ThemedText>
-        <ThemedText>
-          Get tips and strategies for overcoming challenges and enemies.
-        </ThemedText>
-      </ThemedView>
+      <Text style={styles.label}>CONTRASEÑA:</Text>
+      <TextInput
+        style={styles.input}
+        placeholder="Ingresa tu contraseña"
+        placeholderTextColor="#AE9D7F"
+        secureTextEntry
+      />
 
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Bestiary</ThemedText>
-        <ThemedText>
-          Discover information about the creatures you'll face in the dungeon.
-        </ThemedText>
-      </ThemedView>
+      <TouchableOpacity style={styles.button}>
+        <Text style={styles.buttonText}>Ingresar</Text>
+      </TouchableOpacity>
 
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
-        <ThemedText>
-          Edit{" "}
-          <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText>{" "}
-          to see changes. Press{" "}
-          <ThemedText type="defaultSemiBold">
-            {Platform.select({
-              ios: "cmd + d",
-              android: "cmd + m",
-              web: "F12",
-            })}
-          </ThemedText>{" "}
-          to open developer tools.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 2: Explore</ThemedText>
-        <ThemedText>
-          Tap the Explore tab to learn more about what's included in this
-          starter app.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
-        <ThemedText>
-          When you're ready, run{" "}
-          <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText>{" "}
-          to get a fresh <ThemedText type="defaultSemiBold">app</ThemedText>{" "}
-          directory. This will move the current{" "}
-          <ThemedText type="defaultSemiBold">app</ThemedText> to{" "}
-          <ThemedText type="defaultSemiBold">app-example</ThemedText>.
-        </ThemedText>
-      </ThemedView>
-    </ParallaxScrollView>
+      <Text style={styles.footerText}>¿No tienes cuenta? Crea una aquí </Text>
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
-  titleContainer: {
-    flexDirection: "row",
-    alignItems: "center",
-    gap: 8,
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    paddingHorizontal: 30,
     backgroundColor: "#121212",
   },
-  stepContainer: {
-    gap: 8,
-    marginBottom: 8,
+  title: {
+    fontSize: 38,
+    color: "#AE9D7F",
+    marginBottom: 30,
+    textAlign: "center",
+  },
+  label: {
+    fontSize: 20,
+    color: "#AE9D7F",
+    marginBottom: 20,
+  },
+  input: {
     backgroundColor: "#2A2A2A",
+    color: "#FFF",
+    padding: 18,
+    borderRadius: 20,
+    marginBottom: 20,
+    marginLeft:10,
+    marginRight:10,
+    fontSize:17,
   },
-  reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
-    position: "absolute",
+  button: {
+    backgroundColor: "#2A2A2A",
+    padding: 20,
+    borderRadius: 15,
+    alignItems: "center",
+    marginLeft:100,
+    marginRight:100,
+    marginTop:15,
   },
-  titleText: {
-    fontFamily: "Jacques Francois Shadow",
+  buttonText: {
     color: "#AE9D7F",
+    fontSize: 16,
+    fontWeight: "bold",
   },
-  defaultText: {
-    fontFamily: "Cormorant Infant",
-    color: "#AE9D7F",
+  footerText: {
+    marginBottom: 0,
+    marginTop:50,
+    textAlign: "center",
+    color: "#A1CEDC",
+    fontWeight: "bold",
   },
+  
 });
