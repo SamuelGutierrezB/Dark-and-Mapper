9622107fdda6969806ce5690905e9d590ea067d9
samuel6025736 <samuel@nxtagro.io>
2025-03-29 22:03:37 -0600

    Add navigation & fix styles

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,66 +1,72 @@
-import { Image, StyleSheet, TextInput, TouchableOpacity, Text, View,} from "react-native";
-import { BlurView } from 'expo-blur'
-import {useState, useEffect} from "react";
-import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword } from 'firebase/auth';
-import { initializeApp} from 'firebase/app';
-import { firebaseConfig } from '../../firebase-config';
-import * as Font from 'expo-font'
-
+import {
+  Image,
+  StyleSheet,
+  TextInput,
+  TouchableOpacity,
+  Text,
+  View,
+} from "react-native";
+import { useRouter } from "expo-router";
+import { useState, useEffect } from "react";
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
+import { initializeApp } from "firebase/app";
+import { firebaseConfig } from "../../firebase-config";
+import * as Font from "expo-font";
 
 export default function LoginScreen() {
+  const router = useRouter();
 
-  
-
-//inicio de agregación de fuente cormorantinfant
   const [fonstsLoaded, setFontsLoaded] = useState(false);
 
   useEffect(() => {
-    if (!fonstsLoaded){
+    if (!fonstsLoaded) {
       loadFonts();
     }
   });
 
   const loadFonts = async () => {
-
     Font.loadAsync({
-      'cormorantinfant': require('../../assets/fonts/CormorantInfant-Medium.ttf')
+      cormorantinfant: require("../../assets/fonts/CormorantInfant-Medium.ttf"),
     });
 
     setFontsLoaded(true);
-  }
-//Termino de sección de agregación de fuente cormorantinfant
+  };
 
-  const [password, setPassword] = useState('')
-  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState("");
+  const [error, setError] = useState(false);
 
   const app = initializeApp(firebaseConfig);
   const auth = getAuth(app);
-  
+
   const handleSignIn = () => {
     signInWithEmailAndPassword(auth, email, password)
-    .then((userCredential) => { 
-      console.log('Sesion iniciada')
-      const user = userCredential.user; //se ocupa el crear cuenta
-      console.log(user)
-    })
-    .catch(error => {
-      console.log(error)
-    })
-  }
-  
-  
+      .then((userCredential) => {
+        const user = userCredential.user;
+        router.push("/explore");
+      })
+      .catch((error) => {
+        setError(true);
+      });
+  };
+
   return (
     <View style={styles.container}>
-      <Image
-      style={styles.image}
-      source={require('../../assets/images/DarkAndMapper.png')}
-      />
+      <View style={{ alignItems: "center", marginBottom: 20 }}>
+        <Image
+          style={styles.image}
+          source={require("../../assets/images/DarkAndMapper.png")}
+        />
+      </View>
 
       <Text style={styles.title}>Inicia sesión</Text>
-      
+
+      {error && (
+        <label style={styles.error}>Usuario o contraseña erroneos</label>
+      )}
       <Text style={styles.label}>USUARIO:</Text>
-      <TextInput 
-        onChangeText={(text)=> setEmail(text)}
+      <TextInput
+        onChangeText={(text) => setEmail(text)}
         style={styles.input}
         placeholder="Ingresa tu nombre de usuario"
         placeholderTextColor={"#AE9D7F"}
@@ -68,7 +74,7 @@ export default function LoginScreen() {
 
       <Text style={styles.label}>CONTRASEÑA:</Text>
       <TextInput
-        onChangeText={(text)=> setPassword(text)}
+        onChangeText={(text) => setPassword(text)}
         style={styles.input}
         placeholder="Ingresa tu contraseña"
         placeholderTextColor="#AE9D7F"
@@ -79,18 +85,19 @@ export default function LoginScreen() {
         <Text style={styles.buttonText}>Ingresar</Text>
       </TouchableOpacity>
 
-      <Text style={styles.footerText}>¿No tienes cuenta? Crea una aquí </Text>
+      <Text style={styles.footerText} onPress={() => router.push("/register")}>
+        ¿No tienes cuenta? Crea una aquí{" "}
+      </Text>
     </View>
-  )
-};
-
+  );
+}
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: "center",
     paddingHorizontal: 30,
-    paddingVertical:30,
+    paddingVertical: 30,
     backgroundColor: "#121212",
   },
   title: {
@@ -98,13 +105,19 @@ const styles = StyleSheet.create({
     color: "#AE9D7F",
     marginBottom: 30,
     textAlign: "center",
-    fontFamily: 'cormorantinfant',
+    fontFamily: "cormorantinfant",
   },
   label: {
     fontSize: 20,
     color: "#AE9D7F",
     marginBottom: 20,
-    fontFamily: 'cormorantinfant',
+    fontFamily: "cormorantinfant",
+  },
+  error: {
+    fontSize: 20,
+    color: "#F58382",
+    marginBottom: 20,
+    fontFamily: "cormorantinfant",
   },
   input: {
     backgroundColor: "#2A2A2A",
@@ -112,45 +125,36 @@ const styles = StyleSheet.create({
     padding: 18,
     borderRadius: 20,
     marginBottom: 20,
-    marginLeft:10,
-    marginRight:10,
-    fontSize:17,
-    fontFamily: 'cormorantinfant',
-
+    marginLeft: 10,
+    marginRight: 10,
+    fontSize: 17,
+    fontFamily: "cormorantinfant",
   },
   button: {
     backgroundColor: "#2A2A2A",
     padding: 20,
     borderRadius: 15,
     alignItems: "center",
-    marginLeft:100,
-    marginRight:100,
-    marginTop:15,
-
+    marginLeft: 100,
+    marginRight: 100,
+    marginTop: 15,
   },
   buttonText: {
     color: "#AE9D7F",
     fontSize: 16,
     fontWeight: "bold",
-    fontFamily: 'cormorantinfant',
-
+    fontFamily: "cormorantinfant",
   },
   footerText: {
     marginBottom: 0,
-    marginTop:50,
+    marginTop: 50,
     textAlign: "center",
     color: "#A1CEDC",
     fontSize: 17,
-    fontFamily: 'cormorantinfant',
-
+    fontFamily: "cormorantinfant",
   },
-  image:{
-    marginBottom:10,
-    marginLeft:80,
-    marginRight:80,
-    width: 170, 
-    height: 170, 
-  
-  }
-  
+  image: {
+    width: 170,
+    height: 170,
+  },
 });
--- app/(tabs)/register.tsx
+++ app/(tabs)/register.tsx
@@ -1,49 +1,87 @@
-import { Image, StyleSheet, TextInput, TouchableOpacity, Text, View, Alert } from "react-native";
-import { BlurView } from 'expo-blur'
-import {useState, } from "react";
-import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword } from 'firebase/auth';
-import { initializeApp} from 'firebase/app';
+import {
+  Image,
+  StyleSheet,
+  TextInput,
+  TouchableOpacity,
+  Text,
+  View,
+  Alert,
+} from "react-native";
+import { BlurView } from "expo-blur";
+import { useState, useEffect } from "react";
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+} from "firebase/auth";
+import { useRouter } from "expo-router";
+import * as Font from "expo-font";
 
-import { firebaseConfig } from '../../firebase-config';
+import { initializeApp } from "firebase/app";
 
-
+import { firebaseConfig } from "../../firebase-config";
 
 export default function RegisterScreen() {
-  const [password, setPassword] = useState('')
-  const [email, setEmail] = useState('')
+  const router = useRouter();
+
+  const [fonstsLoaded, setFontsLoaded] = useState(false);
+
+  useEffect(() => {
+    if (!fonstsLoaded) {
+      loadFonts();
+    }
+  });
+  const loadFonts = async () => {
+    Font.loadAsync({
+      cormorantinfant: require("../../assets/fonts/CormorantInfant-Medium.ttf"),
+    });
+
+    setFontsLoaded(true);
+  };
+
+  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState("");
+  const [error, setError] = useState(false);
 
   const app = initializeApp(firebaseConfig);
   const auth = getAuth(app);
-  
+
   const handleCreateAccount = () => {
     createUserWithEmailAndPassword(auth, email, password)
-    .then((userCredential) => { 
-      console.log('Cuenta creada')
-      const user = userCredential.user;
-      console.log(user)
-    })
-    .catch(error => {
-      console.log(error)
-      Alert.alert(error.message)
-    })
-  }
-  
-  
+      .then((userCredential) => {
+        const user = userCredential.user;
+        router.push("/explore");
+      })
+      .catch((error) => {
+        setError(true);
+      });
+  };
+
   return (
     <View style={styles.container}>
+      <View style={{ alignItems: "center", marginBottom: 20 }}>
+        <Image
+          style={styles.image}
+          source={require("../../assets/images/DarkAndMapper.png")}
+        />
+      </View>
       <Text style={styles.title}>Crear cuenta</Text>
-      
-      <Text style={styles.label}>USUARIO:</Text>
-      <TextInput 
-        onChangeText={(text)=> setEmail(text)}
+      {error && (
+        <label style={styles.error}>
+          Correo o contraseña no validos para el registro
+        </label>
+      )}
+      <Text style={styles.label}>Correo:</Text>
+      <TextInput
+        onChangeText={(text) => setEmail(text)}
         style={styles.input}
-        placeholder="Ingresa tu nombre de usuario"
+        placeholder="Ingresa tu correo"
         placeholderTextColor={"#AE9D7F"}
       />
 
-      <Text style={styles.label}>CONTRASEÑA:</Text>
+      <Text style={styles.label}>Contraseña</Text>
       <TextInput
-        onChangeText={(text)=> setPassword(text)}
+        onChangeText={(text) => setPassword(text)}
         style={styles.input}
         placeholder="Ingresa contraseña"
         placeholderTextColor="#AE9D7F"
@@ -53,17 +91,16 @@ export default function RegisterScreen() {
       <TouchableOpacity onPress={handleCreateAccount} style={styles.button}>
         <Text style={styles.buttonText}>Crear ingresar</Text>
       </TouchableOpacity>
-
     </View>
-  )
-};
-
+  );
+}
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: "center",
     paddingHorizontal: 30,
+    paddingVertical: 30,
     backgroundColor: "#121212",
   },
   title: {
@@ -71,11 +108,19 @@ const styles = StyleSheet.create({
     color: "#AE9D7F",
     marginBottom: 30,
     textAlign: "center",
+    fontFamily: "cormorantinfant",
   },
   label: {
     fontSize: 20,
     color: "#AE9D7F",
     marginBottom: 20,
+    fontFamily: "cormorantinfant",
+  },
+  error: {
+    fontSize: 20,
+    color: "#F58382",
+    marginBottom: 20,
+    fontFamily: "cormorantinfant",
   },
   input: {
     backgroundColor: "#2A2A2A",
@@ -83,30 +128,36 @@ const styles = StyleSheet.create({
     padding: 18,
     borderRadius: 20,
     marginBottom: 20,
-    marginLeft:10,
-    marginRight:10,
-    fontSize:17,
+    marginLeft: 10,
+    marginRight: 10,
+    fontSize: 17,
+    fontFamily: "cormorantinfant",
   },
   button: {
     backgroundColor: "#2A2A2A",
     padding: 20,
     borderRadius: 15,
     alignItems: "center",
-    marginLeft:100,
-    marginRight:100,
-    marginTop:15,
+    marginLeft: 100,
+    marginRight: 100,
+    marginTop: 15,
   },
   buttonText: {
     color: "#AE9D7F",
     fontSize: 16,
     fontWeight: "bold",
+    fontFamily: "cormorantinfant",
   },
   footerText: {
     marginBottom: 0,
-    marginTop:50,
+    marginTop: 50,
     textAlign: "center",
     color: "#A1CEDC",
     fontSize: 17,
+    fontFamily: "cormorantinfant",
   },
-  
+  image: {
+    width: 170,
+    height: 170,
+  },
 });
