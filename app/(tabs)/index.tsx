efb5b445f98d5006e55bf36c203ceab0bf48a677
Beto <josealberto@ceti.mx>
2025-03-29 20:32:59 -0600

    add fontFamily CormorantInfant & image Logo
    Add function in index to charge the font CormorantInfant and the function of the DarkAndMapper.png

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,14 +1,35 @@
-import { Image, StyleSheet, TextInput, TouchableOpacity, Text, View } from "react-native";
+import { Image, StyleSheet, TextInput, TouchableOpacity, Text, View,} from "react-native";
 import { BlurView } from 'expo-blur'
-import {useState} from "react";
+import {useState, useEffect} from "react";
 import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword } from 'firebase/auth';
 import { initializeApp} from 'firebase/app';
-
 import { firebaseConfig } from '../../firebase-config';
-
+import * as Font from 'expo-font'
 
 
 export default function LoginScreen() {
+
+  
+
+//inicio de agregación de fuente cormorantinfant
+  const [fonstsLoaded, setFontsLoaded] = useState(false);
+
+  useEffect(() => {
+    if (!fonstsLoaded){
+      loadFonts();
+    }
+  });
+
+  const loadFonts = async () => {
+
+    Font.loadAsync({
+      'cormorantinfant': require('../../assets/fonts/CormorantInfant-Medium.ttf')
+    });
+
+    setFontsLoaded(true);
+  }
+//Termino de sección de agregación de fuente cormorantinfant
+
   const [password, setPassword] = useState('')
   const [email, setEmail] = useState('')
 
@@ -30,6 +51,11 @@ export default function LoginScreen() {
   
   return (
     <View style={styles.container}>
+      <Image
+      style={styles.image}
+      source={require('../../assets/images/DarkAndMapper.png')}
+      />
+
       <Text style={styles.title}>Inicia sesión</Text>
       
       <Text style={styles.label}>USUARIO:</Text>
@@ -64,6 +90,7 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: "center",
     paddingHorizontal: 30,
+    paddingVertical:30,
     backgroundColor: "#121212",
   },
   title: {
@@ -71,11 +98,13 @@ const styles = StyleSheet.create({
     color: "#AE9D7F",
     marginBottom: 30,
     textAlign: "center",
+    fontFamily: 'cormorantinfant',
   },
   label: {
     fontSize: 20,
     color: "#AE9D7F",
     marginBottom: 20,
+    fontFamily: 'cormorantinfant',
   },
   input: {
     backgroundColor: "#2A2A2A",
@@ -86,6 +115,8 @@ const styles = StyleSheet.create({
     marginLeft:10,
     marginRight:10,
     fontSize:17,
+    fontFamily: 'cormorantinfant',
+
   },
   button: {
     backgroundColor: "#2A2A2A",
@@ -95,11 +126,14 @@ const styles = StyleSheet.create({
     marginLeft:100,
     marginRight:100,
     marginTop:15,
+
   },
   buttonText: {
     color: "#AE9D7F",
     fontSize: 16,
     fontWeight: "bold",
+    fontFamily: 'cormorantinfant',
+
   },
   footerText: {
     marginBottom: 0,
@@ -107,6 +141,16 @@ const styles = StyleSheet.create({
     textAlign: "center",
     color: "#A1CEDC",
     fontSize: 17,
+    fontFamily: 'cormorantinfant',
+
   },
+  image:{
+    marginBottom:10,
+    marginLeft:80,
+    marginRight:80,
+    width: 170, 
+    height: 170, 
+  
+  }
   
 });
